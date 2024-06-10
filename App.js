216bc21a4a9f666fbe59bd67a483d6141ada79cd
Arseno <pauciloquent.dd@gmail.com>
2024-06-11 05:37:49 +0800

    Describe the changes made in App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Edited on web then edited on vscode</Text>
+      <Text>New line inserted</Text>
       <StatusBar style="auto" />
     </View>
   );
