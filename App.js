71099392a86d819b4ad42af780ecd4fd32c4140c
Dorjoo.bmb <135570000+ArsenoIO@users.noreply.github.com>
2024-06-11 05:17:10 +0800

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>So this is how it is</Text>
+      <Text>Edited on web</Text>
       <StatusBar style="auto" />
     </View>
   );
