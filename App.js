93424a12ab71f0b8c776af01a409c12105d8f4d9
Arseno <pauciloquent.dd@gmail.com>
2024-06-11 06:17:44 +0800

    Added main function UI and added Firebase

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+/*import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -17,5 +17,107 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+*/
+
+import React, { useState } from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { firestore, storage } from './firebaseConfig';
+
+export default function App() {
+  const [shoe, setShoe] = useState({
+    price: '',
+    name: '',
+    code: '',
+    dateRegistered: '',
+    dateSold: '',
+    imageUrl: '',
+    soldPrice: '',
+  });
+
+  const handleInputChange = (name, value) => {
+    setShoe({ ...shoe, [name]: value });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      await firestore().collection('shoes').add(shoe);
+      console.log('Shoe added!');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleImageUpload = async (uri) => {
+    const reference = storage().ref(`/shoes/${new Date().getTime()}.jpg`);
+    await reference.putFile(uri);
+    const imageUrl = await reference.getDownloadURL();
+    handleInputChange('imageUrl', imageUrl);
+  };
+
+  return (
+
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Зургийн линк"
+        value={shoe.imageUrl}
+        onChangeText={(text) => handleInputChange('imageUrl', text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Гутлын үнэ"
+        value={shoe.price}
+        onChangeText={(text) => handleInputChange('price', text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Нэр"
+        value={shoe.name}
+        onChangeText={(text) => handleInputChange('name', text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Гутлын код"
+        value={shoe.code}
+        onChangeText={(text) => handleInputChange('code', text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Бүртгэгдсэн огноо"
+        value={shoe.dateRegistered}
+        onChangeText={(text) => handleInputChange('dateRegistered', text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Зарагдсан огноо"
+        value={shoe.dateSold}
+        onChangeText={(text) => handleInputChange('dateSold', text)}
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Зарагдсан үнэ"
+        value={shoe.soldPrice}
+        onChangeText={(text) => handleInputChange('soldPrice', text)}
+      />
+      <Button title="Нэмэх" onPress={handleSubmit} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingLeft: 8,
   },
 });
---
+++ firebaseConfig.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCj7iJgIwKQTP7Jc-w4GLbPtL6uuPDQ4iY",
+  authDomain: "shoeapp-399ad.firebaseapp.com",
+  projectId: "shoeapp-399ad",
+  storageBucket: "shoeapp-399ad.appspot.com",
+  messagingSenderId: "1070555187722",
+  appId: "1:1070555187722:web:36ffcc5da7b02e16b22df3",
+  measurementId: "G-30SQV4E1XV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
